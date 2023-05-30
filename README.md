e2cf8644d46e39b2d40e0a354aa1880ba7b2007d
David McDonald <26583259+davmmcdonald@users.noreply.github.com>
2023-05-30 15:52:45 -0600

    Added script for Quantum CRG

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # Real Estate Listing Scraper
 
-This is a web scraping script written in Python that extracts data from a website and saves it to a CSV file. It is currently designed to scrape property listings from the Hoff & Leigh website, with additional sites being added. :sunglasses:
+This is a web scraping script written in Python that extracts data from a website and saves it to a CSV file. It is currently designed to scrape property listings from the following brokerages, with additional sites being added. :sunglasses:
+
+- Hoff & Leigh Commercial Real Estate
+- Quamtum Commercial Group
 
 ## Usage
 
 Make sure you have Python 3 installed on your system. The script requires the following command-line arguments:
 
-`python3 ls-hoff-leigh.py <url> <output.csv>`
+`python3 <script>.py <url> <output>.csv`
 
-- `<url>`: The URL of the LS Hoff-Leigh website to scrape data from.
+- `<url>`: The URL of the web page to scrape data from.
 - `<output.csv>`: The name of the output CSV file to save the scraped data.
 
 Before running the script, ensure that you have the necessary packages installed. You can install the required packages by running the following command:
 
-```pip install beautifulsoup4
+`pip install beautifulsoup4`
--- ls-quantum.py
+++ ls-quantum.py
@@ -10,13 +10,14 @@ from datetime import datetime
 # Check if the required arguments are provided
 if len(sys.argv) < 3:
     print('Error: Expected at least two arguments.')
-    print("Usage: python3 ls-hoff-leigh.py <url> <output.csv>")
+    print("Usage: python3 ls-quantum.py <url> <output.csv>")
     sys.exit(1)
 
 # Function to format a string by removing HTML tags and extra spaces
 def format_string(old_str):
     new_str = re.sub('<.*?>', '', old_str)  # Remove HTML tags
     new_str = ' '.join(new_str.split()).replace('\t', '').replace('\n', ',')  # Remove extra spaces, tabs, and newlines
+    new_str = new_str.replace('&amp;', '&').replace('‚Äì', '-') # Replaces some incorrectly displayed characters
     return new_str
 
 # Function to split an address into street and city/state/zip parts
@@ -36,11 +37,12 @@ url = args[1]  # URL to scrape data from
 csv_filename = args[2]  # Name of the output CSV file
 current_directory = os.getcwd()
 current_date = datetime.now().date().strftime("%Y-%m-%d")
-output_folder = "output/hoff-leigh"
+output_folder = "output/quantum"
 
-if '.csv' not in csv_filename: # Adds .csv extension to file, if not specified by user
+# Adds .csv extension to file, if not specified by user
+if '.csv' not in csv_filename: 
     csv_filename = csv_filename + '.csv'
-
+    
 csv_path = os.path.join(current_directory, output_folder, current_date, csv_filename)
 
 # Open the URL and read the HTML content
@@ -49,12 +51,9 @@ html = page.read().decode('utf-8')
 soup = BeautifulSoup(html, 'html.parser')
 
 # Find relevant elements using their classes
-addresses = soup.find_all(class_='listings-results-address')
-prices = soup.find_all(class_='listings-results-price')
-types = soup.find_all(class_='listings-results-proptype')
-sizes = soup.find_all(class_='listings-results-sqft')
-urls = soup.find_all(class_='listings-results-result')
-flyers = soup.find_all(class_='listings-results-document')
+addresses = soup.find_all(class_='property-address')
+titles = soup.find_all(class_='property-details-title')
+flyers = soup.find_all(lambda tag: tag.name == 'a' and tag.get_text() == 'Download Brochure')
 
 # Create the directory if it doesn't exist
 os.makedirs(os.path.dirname(csv_path), exist_ok=True)
@@ -62,18 +61,15 @@ os.makedirs(os.path.dirname(csv_path), exist_ok=True)
 # Open the CSV file and write header row
 with open(csv_path, 'w', newline='') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(['Address', 'City, State, Zip', 'Sale Price', 'Building Type', 'Building Size', 'Listing URL', 'Flyer URL'])
+    writer.writerow(['Address', 'City, State, Zip', 'Listing Title', 'Flyer URL'])
     
     # Iterate over the scraped data and write rows to the CSV file
     for i in range(len(addresses)):
         address, city_state_zip = split_address(addresses[i].prettify().strip())
-        price = format_string(prices[i].get_text().strip())
-        building_type = format_string(types[i].get_text().strip())
-        building_size = format_string(sizes[i].get_text().strip())
-        listing_url = urls[i]['href']
+        title = format_string(titles[i].get_text().strip())
         flyer_url = flyers[i]['href']
 
-        writer.writerow([address, city_state_zip, price, building_type, building_size, listing_url, flyer_url])
+        writer.writerow([address, city_state_zip, title, flyer_url])
 
 # Print the number of listings scraped and the path of the CSV file
 print(len(addresses), 'listings scraped!')
